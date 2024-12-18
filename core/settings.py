e144c04a583815ef523c6d7942f03a2df3cc259c
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-12-18 19:50:38 +0600

    removed the apikey to .env

--- core/settings.py
+++ core/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from decouple import config
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o!0+-@5$$%ye-*)x8n^3jo)@pcs^-m!20*eig&6d(a(up)=y3u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,7 +125,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-import os
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
