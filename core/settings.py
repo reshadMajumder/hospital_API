5772bfc89fc9c2732b5aaf682d1c52980a29a1cd
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-12-21 01:00:30 +0600

    cors origin update

--- core/settings.py
+++ core/settings.py
@@ -80,7 +80,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:5173',
-    'https://hospital-rose-three.vercel.app/'
+    'https://hospital-rose-three.vercel.app'
 ]
 
 # Database
